a507cf90dde0f1d05efba3addd751c9608d7e9bf
AntTAKEOVER <vedantpatwari01@gmail.com>
2018-01-20 23:28:21 +0530

    BIG Change
    -

---
+++ ArtificialTankDriver/Assets/MyScripts/PreLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PreLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (this.gameObject);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator loadNextScene(){
+		
+		yield return new WaitForSeconds (2f);
+		SceneManager.LoadScene ("Main");
+	}
+}
